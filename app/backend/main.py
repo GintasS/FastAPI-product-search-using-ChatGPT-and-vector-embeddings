286d2b82ad37c241b90370a7a9bf8d5ae6aaaf6a
GintasS <svedgintas@gmail.com>
2024-09-29 20:01:03 -0700

    Added ability to generate product embeddings from main.

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,7 +1,8 @@
 from core.config import app
-import os
-
+from database.data_helpers import get_product_embeddings
 import uvicorn
 
+from api.endpoints import handle_new_chat_message_from_user, get_products_by_description
 if __name__ == "__main__":
+  #get_product_embeddings()
   uvicorn.run(app, host="0.0.0.0", port=8000)
