083065a33a8cffd9254a8fd36eb9a99fb5fea70a
GintasS <svedgintas@gmail.com>
2024-09-29 20:34:49 -0700

    Removed unused API method.

--- app/backend/main.py
+++ app/backend/main.py
@@ -3,7 +3,7 @@ from database.data_helpers import get_product_embeddings
 import uvicorn
 
 # Registering API endpoints.
-from api.endpoints import handle_new_chat_message_from_user, get_products_by_description
+from api.endpoints import handle_new_chat_message_from_user
 
 if __name__ == "__main__":
   #get_product_embeddings()
