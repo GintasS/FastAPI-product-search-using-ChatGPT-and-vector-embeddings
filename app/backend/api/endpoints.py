f01c6af00bb333257ed0115c49d9a22e14752613
GintasS <svedgintas@gmail.com>
2024-10-05 13:34:21 -0700

    Refactored the main chat endpoint and added few new endpoints.

--- app/backend/api/endpoints.py
+++ app/backend/api/endpoints.py
@@ -1,31 +1,61 @@
 from core.config import app
-from baml_handlers.categorize_chat_message_handler import categorize_user_message
-from baml_handlers.extract_product_description_from_chat_message import extract_product_description_from_message
-from baml_handlers.extract_product_price_from_chat_message import extract_product_price_from_message
-
+from baml_handlers.extract_product_information_from_message_handler import extract_product_information_from_message
 from models.request.create_chat_message_request import CreateChatMessageRequest
 from models.response.create_chat_message_response import CreateChatMessageResponse
-from baml_client.types import MessageCategory
+from models.request.get_product_price_range_request import GetProductPriceRangeRequest
 from core.similarity_helper import *
 from database.data_helpers import *
 from database.in_memory_db import products_embeddings
 from core.constants import AssistantMessages
 
 @app.post("/v1/chat/message/")
-async def handle_new_chat_message_from_user(new_message : CreateChatMessageRequest):
-  message_category = await categorize_user_message(new_message.text)
-  response_string = AssistantMessages.ASSISTANT_MESSAGE_SORRY_MESSAGE
+async def handle_new_chat_message_from_user(request: CreateChatMessageRequest):
+  extracted_chat_gpt_request = await extract_product_information_from_message(request.text)
+  product_description = extracted_chat_gpt_request.description
+  product_price_range = extracted_chat_gpt_request.price_range
+  products = products_embeddings
+  filter_was_active = False
+  
+  if is_product_description_valid(product_description):
+    products = get_similarity_df(product_description, products, request.similarities_results_max_count, request.similarities_results_threshold, OPENAI.EMBEDDING_MODEL) 
+    filter_was_active = True
 
-  if message_category is MessageCategory.QuestionAboutProductDescription:
-    product_description_by_user = await extract_product_description_from_message(new_message.text)
-    similarity_df = get_similarity_dataframe(product_description_by_user.text, products_embeddings, General.GET_TOP_X_RESULTS_FROM_SIMILARITY_DATAFRAME)
-    products_with_titles_and_prices = get_products_with_prices_string(similarity_df)
-    response_string = AssistantMessages.ASSISTANT_MESSAGE_RECOMMENDED_PRODUCTS.format(products_with_titles_and_prices)
+  if is_price_range_valid(product_price_range):
+    products = apply_mask_for_df(products, product_price_range)
+    filter_was_active = True
+   
+  if len(products) is 0 or filter_was_active is False:
+    return CreateChatMessageResponse(text=AssistantMessages.ASSISTANT_MESSAGE_PRODUCT_SIMILARITY_SEARCH_FAILURE)
+  
+  product_summary = get_products_summary(products)
+  return CreateChatMessageResponse(text=product_summary)
 
-  elif message_category is MessageCategory.QuestionAboutProductPrice:
-    product_price_masks = await extract_product_price_from_message(new_message.text)
-    products_applied_price_mask = apply_mask_for_dataframe(products_embeddings, product_price_masks)
-    products_with_prices_string = get_products_with_prices_string(products_applied_price_mask)
-    response_string = AssistantMessages.ASSISTANT_MESSAGE_PRODUCTS_BETWEEN_PRICE.format(products_with_prices_string)
+@app.post("/v1/chat/product/description/")
+async def handle_product_description_request(request: CreateChatMessageRequest):
+  extracted_chat_gpt_request = await extract_product_information_from_message(request.text)
+  product_description = extracted_chat_gpt_request.description
 
-  return CreateChatMessageResponse(text=response_string)
+  if not is_product_description_valid(product_description):
+    return CreateChatMessageResponse(text=AssistantMessages.ASSISTANT_MESSAGE_INVALID_DESCRIPTION)
+
+  products = get_similarity_df(product_description, products_embeddings, request.similarities_results_max_count, request.similarities_results_threshold, OPENAI.EMBEDDING_MODEL)
+  if len(products) == 0:
+    return CreateChatMessageResponse(text=AssistantMessages.ASSISTANT_MESSAGE_PRODUCT_SIMILARITY_SEARCH_FAILURE)
+
+  product_summary = get_products_summary(products)
+  return CreateChatMessageResponse(text=product_summary)
+
+@app.post("/v1/chat/product/price/")
+async def handle_product_price_range_request(request: GetProductPriceRangeRequest):
+  extracted_chat_gpt_request = await extract_product_information_from_message(request.text)
+  product_price_range = extracted_chat_gpt_request.price_range
+
+  if not is_price_range_valid(product_price_range):
+    return CreateChatMessageResponse(text=AssistantMessages.ASSISTANT_MESSAGE_INVALID_PRICE_RANGE)
+
+  products = apply_mask_for_df(products_embeddings, product_price_range)
+  if len(products) == 0:
+    return CreateChatMessageResponse(text=AssistantMessages.ASSISTANT_MESSAGE_PRODUCT_SIMILARITY_SEARCH_FAILURE)
+
+  product_summary = get_products_summary(products)
+  return CreateChatMessageResponse(text=product_summary)
