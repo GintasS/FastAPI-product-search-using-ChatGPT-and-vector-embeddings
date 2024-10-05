fbe0ceb8600020edd880b4be287ba268bc150307
GintasS <svedgintas@gmail.com>
2024-10-05 15:26:51 -0700

    Added request validation.

--- app/backend/models/request/create_chat_message_request.py
+++ app/backend/models/request/create_chat_message_request.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class CreateChatMessageRequest(BaseModel):
-    text: str
+  text: str
+  similarities_results_max_count : int  = Field(default=5)
+  similarities_results_threshold : float = Field(gr=0.5, default=0.8)
